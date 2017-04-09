a1a4db9675445685ebe58c822ab7a5a79c63b87d
milaijiang <1015389283@qq.com>
2017-04-09 16:11:41 +0800

    0.1.3
    怼不动

---
+++ js/shuaibi.js
@@ -0,0 +1,10 @@
+function newlink()
+{
+	window.open("http://www.bilibili.com/video/av2443259/?from=search&seid=5635828600104308545");
+};
+function mouseEnter(a){
+	a.play()
+}
+function mouseOut(a){
+	a.pause()
+}
