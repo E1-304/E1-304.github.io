4fe507f7a37b7e694473e5cd9d5d3c434537b3df
todaylg <2489437918@qq.com>
2017-04-26 22:00:44 +0800

    Fix Jia’s Some Bug

--- js/main.js
+++ js/main.js
@@ -168,16 +168,131 @@ document.getElementById("fei").onmouseover = function() {
 			document.body.removeChild(first);
 		},1500);
 	}
+	var Jia_Anima = true;
 	document.getElementById("jia").onclick = function() {
+		if(Jia_Anima){
+			Jia_Anima = false;
+			particlesJS("particles-js", {
+  "particles": {
+    "number": {
+      "value": 200,
+      "density": {
+        "enable": true,
+        "value_area": 800
+      }
+    },
+    "color": {
+      "value": "#ffffff"
+    },
+    "shape": {
+      "type": "image",
+      "stroke": {
+        "width": 3,
+        "color": "#fff"
+      },
+      "polygon": {
+        "nb_sides": 5
+      },
+      "image": {
+        "src": "http://www.dynamicdigital.us/wp-content/uploads/2013/02/starburst_white_300_drop_2.png",
+        "width": 100,
+        "height": 100
+      }
+    },
+    "opacity": {
+      "value": 0.7,
+      "random": false,
+      "anim": {
+        "enable": false,
+        "speed": 1,
+        "opacity_min": 0.1,
+        "sync": false
+      }
+    },
+    "size": {
+      "value": 5,
+      "random": true,
+      "anim": {
+        "enable": false,
+        "speed": 20,
+        "size_min": 0.1,
+        "sync": false
+      }
+    },
+    "line_linked": {
+      "enable": false,
+      "distance": 50,
+      "color": "#ffffff",
+      "opacity": 0.6,
+      "width": 1
+    },
+    "move": {
+      "enable": true,
+      "speed": 5,
+      "direction": "bottom",
+      "random": true,
+      "straight": false,
+      "out_mode": "out",
+      "bounce": false,
+      "attract": {
+        "enable": true,
+        "rotateX": 300,
+        "rotateY": 1200
+      }
+    }
+  },
+  "interactivity": {
+    "detect_on": "canvas",
+    "events": {
+      "onhover": {
+        "enable": true,
+        "mode":  "bubble"
+      },
+      "onclick": {
+        "enable": true,
+        "mode": "repulse"
+      },
+      "resize": true
+    },
+    "modes": {
+      "grab": {
+        "distance": 150,
+        "line_linked": {
+          "opacity": 1
+        }
+      },
+      "bubble": {
+        "distance": 200,
+        "size": 40,
+        "duration": 2,
+        "opacity": 8,
+        "speed": 3
+      },
+      "repulse": {
+        "distance": 200,
+        "duration": 0.2
+      },
+      "push": {
+        "particles_nb": 4
+      },
+      "remove": {
+        "particles_nb": 2
+      }
+    }
+  },
+  "retina_detect": true
+});
+		}
+		document.getElementById("Jia_box").style.display='block';
         document.getElementById("jia").classList.remove("little_box_jia","jia_transform");
 		document.getElementById("jia").classList.add("big_box_jia","jia_block");
 		document.getElementById("jia_name").classList.add("footer_name_disappear");
 		$("#jia_img1").animate({opacity:1},2000, function() {
-        	$("#jia_img2").animate({opacity:1},1100, function() {
-          		$("#jia_img3").animate({opacity:1},1320, function() {
-          			$("#jia_img4").animate({opacity:1},900, function() {
-          				$("#jia_img5").animate({opacity:1},800, function() {
-          					$("#jia_img6").animate({opacity:1},1000, function() {})
+        	$("#jia_img2").animate({opacity:1},500, function() {
+          		$("#jia_img3").animate({opacity:1},620, function() {
+          			$("#jia_img4").animate({opacity:1},500, function() {
+          				$("#jia_img5").animate({opacity:1},500, function() {
+          					$("#jia_img6").animate({opacity:1},500, function() {})
           				})
           			})
           		})
@@ -293,17 +408,18 @@ document.getElementById("fei").onmouseover = function() {
 	}
 	document.getElementById("jia_return").onclick = function(e) {
 		e.stopPropagation();//事件冒泡解决方法		
-		$("#jia_img1").animate({opacity:0},500, function() {
-        	$("#jia_img2").animate({opacity:0},400, function() {
-          		$("#jia_img3").animate({opacity:0},620, function() {
-          			$("#jia_img4").animate({opacity:0},500, function() {
-          				$("#jia_img5").animate({opacity:0},900, function() {
-          					$("#jia_img6").animate({opacity:0},800, function() {
+		$("#jia_img1").animate({opacity:0},800, function() {
+        	$("#jia_img2").animate({opacity:0},200, function() {
+          		$("#jia_img3").animate({opacity:0},320, function() {
+          			$("#jia_img4").animate({opacity:0},250, function() {
+          				$("#jia_img5").animate({opacity:0},220, function() {
+          					$("#jia_img6").animate({opacity:0},210, function() {
           						setTimeout(function(){
 								document.getElementById("jia").classList.remove("big_box_jia","jia_block");
 								document.getElementById("jia").classList.add("little_box_jia","jia_transform");
 								// document.getElementsByClassName("onclick_before")[4].classList.remove("onclick_after");
 								document.getElementById("jia_name").classList.remove("footer_name_disappear");
+								document.getElementById("Jia_box").style.display='none';
 							},700);
 							document.getElementsByClassName("personal_statement")[4].classList.add("fallbackAnimation");
 							function addNewDiv(){
